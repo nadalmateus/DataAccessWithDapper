3a085f6b05038b2b4253f1f211000811dffa8036
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-24 23:48:38 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace data_access_dapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
