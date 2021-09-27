4f4341dc294ca517f503fe15bded3a6e988f73c8
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-27 17:51:42 -0300

    connectionString

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace data_access_dapper
     {
         static void Main(string[] args)
         {
+            const string connectionString = "Server=localhost,1433;Database=data;User ID=sa;Password=1q2w3e4r@#$";
             Console.WriteLine("Hello World!");
         }
     }
