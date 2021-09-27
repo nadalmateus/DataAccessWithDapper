54b50ded45b512c0f83a70b61ad91be52c2c3081
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-27 20:54:52 -0300

    SqlCommand and SqlDataReader

--- Program.cs
+++ Program.cs
@@ -8,9 +8,21 @@ namespace data_access_dapper
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=data;User ID=sa;Password=1q2w3e4r@#$";
-
             using var connection = new SqlConnection(connectionString);
-            Console.WriteLine("Conectado");
+
+            connection.Open();
+            using var command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandType = System.Data.CommandType.Text;
+            command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+            }
+
         }
     }
 }
