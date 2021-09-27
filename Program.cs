1c6a4f319c5d117d9f668285d0aba1134f522272
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-27 19:22:53 -0300

    using

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Data.SqlClient;
 
 namespace data_access_dapper
 {
@@ -7,7 +8,9 @@ namespace data_access_dapper
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=data;User ID=sa;Password=1q2w3e4r@#$";
-            Console.WriteLine("Hello World!");
+
+            using var connection = new SqlConnection(connectionString);
+            Console.WriteLine("Conectado");
         }
     }
 }
