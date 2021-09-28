f557654cebac0c0dd2019fdeb6a6f39fcce17170
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-27 21:15:51 -0300

    using dapper

---
+++ Models/Category.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace data_access_dapper.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,20 +9,6 @@ namespace data_access_dapper
         {
             const string connectionString = "Server=localhost,1433;Database=data;User ID=sa;Password=1q2w3e4r@#$";
             using var connection = new SqlConnection(connectionString);
-
-            connection.Open();
-            using var command = new SqlCommand();
-            command.Connection = connection;
-            command.CommandType = System.Data.CommandType.Text;
-            command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-            var reader = command.ExecuteReader();
-
-            while (reader.Read())
-            {
-                Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-            }
-
         }
     }
 }
