974a1cbb14ee532b8b3c2c8ac0256b855e27c89a
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-27 21:22:21 -0300

    first GET

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Dapper;
+using data_access_dapper.Models;
 using Microsoft.Data.SqlClient;
 
 namespace data_access_dapper
@@ -9,6 +11,11 @@ namespace data_access_dapper
         {
             const string connectionString = "Server=localhost,1433;Database=data;User ID=sa;Password=1q2w3e4r@#$";
             using var connection = new SqlConnection(connectionString);
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var category in categories)
+            {
+                Console.WriteLine($"{category.Id},{category.Title}");
+            }
         }
     }
 }
