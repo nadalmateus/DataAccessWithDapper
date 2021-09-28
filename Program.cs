c730409e69abd2c6f035b3d562a1c955c3de21af
Mateus Nadal <nadalmateus@hotmail.com>
2021-09-27 21:56:07 -0300

    SqlParameter, insert

--- Program.cs
+++ Program.cs
@@ -11,18 +11,31 @@ namespace data_access_dapper
         {
             const string connectionString = "Server=localhost,1433;Database=data;User ID=sa;Password=1q2w3e4r@#$";
 
-            var category = new Category();
-            category.Id = Guid.NewGuid();
-            category.Title = "AWS";
-            category.Url = "aws.com";
-            category.Description = "Cloud";
-            category.Order = 8;
-            category.Summary = "AWL Cloud";
-            category.Featured = false;
+            var category = new Category
+            {
+                Description = "Cloud",
+                Id = Guid.NewGuid(),
+                Title = "AWS",
+                Url = "aws.com",
+                Order = 8,
+                Summary = "AWL Cloud",
+                Featured = false
+            };
 
-            var insertSql = @"INSERT INTO [Category] VALUES(NEWID(), title, url, summary, order, description, featured)";
+            var insertSql = @"INSERT INTO [Category] VALUES(@id, @Title, @Url, @Summary, @Order, @Description, @Featured)";
 
             using var connection = new SqlConnection(connectionString);
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Description,
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Order,
+                category.Summary,
+                category.Featured
+            });
+            Console.WriteLine($"{rows} linhas inseridas");
 
             var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
             foreach (var item in categories)
